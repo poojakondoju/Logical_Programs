dd95129f8fdf87f5ec213c3b1f1428182c024bf1
poojakondoju <kondojupoojitha612@gmail.com>
2021-08-24 12:16:42 +0530

    logical programs

---
+++ Reverse_number.java
@@ -0,0 +1,21 @@
+package logicalprograms;
+
+import java.util.Scanner;
+
+public class Reverse_number{
+
+	public static void main(String[] arg)
+	{
+	int a,res=0,n;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter a number");
+	n=sc.nextInt();
+	for( ;n!=0; )
+	{
+	a=n%10;
+	res=(res*10)+a;
+	n=n/10;
+	}
+	System.out.println("Reverse of a number is =  "+res);
+	}
+}
