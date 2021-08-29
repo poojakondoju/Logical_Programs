01024fa3e6cb3dd0c7ad7a5273eee03c06acae1d
poojakondoju <kondojupoojitha612@gmail.com>
2021-08-29 21:17:32 +0530

    stopwatch program

--- Stopwatch.java
+++ Stopwatch.java
@@ -2,48 +2,43 @@ package logicalprograms;
 
 import java.util.Scanner;
 
-public class Stopwatch 
-{
-	public long startTimer=0;
-	public long stopTimer=0;
+public class Stopwatch {
+	public long startTimer = 0;
+	public long stopTimer = 0;
 	public long elapsed;
-	
-	//to start timer
-	public void start()
-	{
-		startTimer=System.currentTimeMillis();
-		System.out.println("Start Time is: "+startTimer);
+
+	// to start timer
+	public void start() {
+		startTimer = System.currentTimeMillis();
+		System.out.println("Start Time is: " + startTimer);
 	}
-	
+
 	// to stop timer
-	public void stop()
-	{
-		stopTimer=System.currentTimeMillis();
-		System.out.println("Stop Time is: "+stopTimer);
+	public void stop() {
+		stopTimer = System.currentTimeMillis();
+		System.out.println("Stop Time is: " + stopTimer);
 	}
-	
-	public long getElapsedTime()
-	{
-		elapsed=stopTimer-startTimer;
+
+	public long getElapsedTime() {
+		elapsed = stopTimer - startTimer;
 		return elapsed;
 	}
-	
-	public static void main(String[] args) throws Exception
-	{
-		Stopwatch sw=new Stopwatch();
-		
+
+	public static void main(String[] args) throws Exception {
+		Stopwatch sw = new Stopwatch();
+
 		System.out.println("Press '1' to Start Time: ");
-		
+
 		sw.start();
 
 		System.out.println();
 		System.out.println("Press '2' to Stop Time: ");
 		sw.stop();
 
-		long l=sw.getElapsedTime();
+		long l = sw.getElapsedTime();
 		System.out.println();
-		System.out.println("Total Time Elapsed(in millisec) is:"+l);
+		System.out.println("Total Time Elapsed(in millisec) is:" + l);
 		System.out.println();
-		System.out.println("Converting millisec to seconds: "+(l/1000)+" sec");
+		System.out.println("Converting millisec to seconds: " + (l / 1000) + " sec");
 	}
 }
