33987d73c51720936e598c4df979a9a5f172a4ad
poojakondoju <kondojupoojitha612@gmail.com>
2021-08-29 21:21:35 +0530

    print prime number

--- primenumber.java
+++ primenumber.java
@@ -1,29 +1,27 @@
 package logicalprograms;
 
-   import java.util.Scanner;
-  
-   public class primenumber {
+import java.util.Scanner;
+
+public class primenumber {
 
 	public static void main(String[] args) {
-				int num = 0;
+		int num = 0;
 
-				boolean flag = false;
-				System.out.println("Enter your number: ");
-				try (Scanner sc = new Scanner(System.in)) {
-					num = sc.nextInt();
-				}
-				for (int i = 2; i <= num / 2; i++) {
-					if (num % i == 0)
-					{
-						flag = true;
-						break;
-					}
-					
-					}
-					if (!flag)
-						System.out.println("Prime number is " + num);
-					else
-						System.out.println( num + " its a not prime number");
+		boolean flag = false;
+		System.out.println("Enter your number: ");
+		try (Scanner sc = new Scanner(System.in)) {
+			num = sc.nextInt();
 		}
-		}
+		for (int i = 2; i <= num / 2; i++) {
+			if (num % i == 0) {
+				flag = true;
+				break;
+			}
 
+		}
+		if (!flag)
+			System.out.println("Prime number is " + num);
+		else
+			System.out.println(num + " its a not prime number");
+	}
+}
