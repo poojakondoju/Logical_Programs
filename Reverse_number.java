c05caff8be4235b4b180da1a4630022f6ebcfb63
poojakondoju <kondojupoojitha612@gmail.com>
2021-08-29 21:15:58 +0530

    print Reverse number

--- Reverse_number.java
+++ Reverse_number.java
@@ -2,20 +2,18 @@ package logicalprograms;
 
 import java.util.Scanner;
 
-public class Reverse_number{
+public class Reverse_number {
 
-	public static void main(String[] arg)
-	{
-	int a,res=0,n;
-	Scanner sc=new Scanner(System.in);
-	System.out.println("Enter a number");
-	n=sc.nextInt();
-	for( ;n!=0; )
-	{
-	a=n%10;
-	res=(res*10)+a;
-	n=n/10;
-	}
-	System.out.println("Reverse of a number is =  "+res);
+	public static void main(String[] arg) {
+		int a, res = 0, n;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		n = sc.nextInt();
+		for (; n != 0;) {
+			a = n % 10;
+			res = (res * 10) + a;
+			n = n / 10;
+		}
+		System.out.println("Reverse of a number is =  " + res);
 	}
 }
