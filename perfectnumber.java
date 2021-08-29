5492753c626de09a0a6076344f78f4734d59ca61
poojakondoju <kondojupoojitha612@gmail.com>
2021-08-29 21:20:05 +0530

    print perfectnumber

--- perfectnumber.java
+++ perfectnumber.java
@@ -1,31 +1,24 @@
 package logicalprograms;
 
-   import java.util.Scanner;
+import java.util.Scanner;
 
-   public class perfectnumber {
-		
-	   public static void main(String[] args) 
-			    {
-			        int n, sum = 0;
-			        try (Scanner sc = new Scanner(System.in)) {
-						System.out.print("Enter any integer you want to check:");
-						n = sc.nextInt();
-					}
-			        for(int i = 1; i < n; i++)
-			        {
-			            if(n % i == 0)
-			            {
-			                sum = sum + i;
-			            }
-			        }
-			        if(sum == n)
-			        {
-			            System.out.println(n + " " + " -:Given number is Perfect");
-			        }
-			        else
-			        {
-			            System.out.println(n + " " +  "-:Given number is not Perfect");
-			        }    
-		}
-		}
+public class perfectnumber {
 
+	public static void main(String[] args) {
+		int n, sum = 0;
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.print("Enter any integer you want to check:");
+			n = sc.nextInt();
+		}
+		for (int i = 1; i < n; i++) {
+			if (n % i == 0) {
+				sum = sum + i;
+			}
+		}
+		if (sum == n) {
+			System.out.println(n + " " + " -:Given number is Perfect");
+		} else {
+			System.out.println(n + " " + "-:Given number is not Perfect");
+		}
+	}
+}
